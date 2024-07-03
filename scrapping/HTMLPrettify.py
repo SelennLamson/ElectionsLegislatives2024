6576df14fca053bc0f5c1666e56a18c40cd3640a
Clev <101217516+Clevyyy@users.noreply.github.com>
2024-07-03 14:48:23 +0200

    Update HTMLPrettify.py

--- scrapping/HTMLPrettify.py
+++ scrapping/HTMLPrettify.py
@@ -1,13 +1,13 @@
 from bs4 import BeautifulSoup
 
-with open('scrapping/data.html', 'r', encoding='utf-8') as file:
+with open('scrapping/webdata/data.html', 'r', encoding='utf-8') as file:
     html_content = file.read()
 
 soup = BeautifulSoup(html_content, 'html.parser')
 
 formatted_html = soup.prettify()
 
-with open('scrapping/formatted_table.html', 'w', encoding='utf-8') as file:
+with open('scrapping/webdata/formatted_table.html', 'w', encoding='utf-8') as file:
     file.write(formatted_html)
 
 print("Le fichier HTML a été formaté et sauvegardé dans 'formatted_table.html'.")
